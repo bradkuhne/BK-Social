b564bca8d77e5867d73d68b9df16c8aca424a2f1
Brad Kuhne <bjkuhne@aol.com>
2022-03-20 15:08:01 -0500

    completed User.js model.  Need to add thought count to thought model when I create the Thought model.

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+
+// Define the UserSchema
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a user name!',
+        trim: true
+    },
+    email: {
+        type: String,
+        match: [/.+\@.+\..+/],
+        required: true,
+        unique: true,
+        trim: true
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+    ],
+    friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+//export the User model
+module.exports = User;
